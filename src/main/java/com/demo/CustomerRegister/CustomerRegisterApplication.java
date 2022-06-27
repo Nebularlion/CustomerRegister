9cf79d84dff86d5134a45621c94149346b45a60c
Elina Rajala <elina.rajala@flowhaven.com>
2022-06-28 00:17:09 +0300

    Initial commit

---
+++ src/main/java/com/demo/CustomerRegister/CustomerRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.demo.CustomerRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/CustomerRegister/CustomerRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.CustomerRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
